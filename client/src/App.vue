5682d855a357ad82262c05fb5a36539a67333365
Chrishaun Jones <ckjones1@student.fullsail.edu>
2025-05-28 13:15:30 -0400

    installed analytics package to see traffic

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <UApp>
-      <Analytics mode="production" />
-      <Analytics debug="true" />
+      <Analytics mode="production" debug="true" />
+
       <RouterView />
       <AppFooter />
     </UApp>
